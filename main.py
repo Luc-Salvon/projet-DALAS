8e56bd9fb550b86a33a774f71b890c43bb70b77d
Raphaël R <93404597+Raphael-Renard@users.noreply.github.com>
2024-01-25 19:09:31 +0100

    correction get_platform et ajout de get_title

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from urllib import request
 base_url = "https://howlongtobeat.com/game/"
 
 
+def get_title(page):
+    title = page.find("title").text
+    title = title[12:].split('?')[0]
+    return title
+
 def get_rating(page):
     rating_tag = page.find("a", attrs={"class": "text_primary", "href": re.compile("/game/[0-9]+/reviews")})
     if rating_tag is None:
@@ -17,6 +22,15 @@ def get_rating(page):
     rating = rating_tag.text.split("%")[0]
     return rating
 
+def get_platform(page):
+    classes = page.find_all("div","GameSummary_profile_info__HZFQu GameSummary_medium___r_ia")
+    for classe in classes:
+        if "Platform" in classe.find("strong").text:
+            platform = classe.text
+            platform = platform.split(":")[1][1:]
+            platform = platform.split(', ')
+            return platform
+    return ["NP"] # pas de plateforme renseignee
 
 # 108288
 for i in range(60000, 60011):
@@ -26,16 +40,11 @@ for i in range(60000, 60011):
     except urllib.error.HTTPError:
         continue
 
-    print(page.find("title"))
+    print(get_title(page))
     print(get_rating(page))
+    print(get_platform(page))
+    print("")
 
 
 
-def get_platform(page):
-    classes = page.find_all("div","GameSummary_profile_info__HZFQu GameSummary_medium___r_ia")
 
-    for classe in classes:
-        if "Platform" in classe.find("strong").text:
-            platform = classe.text
-            platform = platform.split(":")[1][1:]
-            return platform
