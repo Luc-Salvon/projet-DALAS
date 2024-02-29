93c80700a31c9355e3a5dd619b2767ac00c769f9
Luc Salvon <salvon.luc@gmail.com>
2024-02-29 17:38:47 +0100

    Traitement prix

--- Scrapping/clean-csv.py
+++ Scrapping/clean-csv.py
@@ -1,8 +1,41 @@
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv("../Donnees/game_data.csv")
 df = df.set_index("hltb_id")
 
 
+# Prix
+def traitement_prix(prix):
+    prix = str(prix)
+
+    if prix == "nan":
+        return
+
+    if prix[0] == '$':
+        if len(prix) == 1:
+            return
+        return float(prix[1:]) * .92
+
+    if prix[-1] == "€":
+        return float(prix[:-1].replace(",", ".").replace("-", "0"))
+
+    if prix in {"Gratuit", "Free-to-play", "Free to Play", "Install Now", "Free To Play", "Gratuit !", "Free Mod"}:
+        return 0.
+
+    print(f"PRIX NON TRAITE : {prix}")
+    return
+
+
+def traitement(dfcol, f):
+    remplacement = np.zeros(len(dfcol))
+
+    for i, el in enumerate(dfcol):
+        remplacement[i] = f(el)
+
+    return remplacement
+
+
+df["price"] = traitement(df["price"], traitement_prix)
 
 df.to_csv("../Donnees/cleaned_data.csv")
