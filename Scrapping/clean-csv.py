261c8dd7ac290e4a7d9c42f80728e63276a00b61
Luc Salvon <salvon.luc@gmail.com>
2024-03-13 23:01:12 +0100

    Ajout traitements

--- Scrapping/clean-csv.py
+++ Scrapping/clean-csv.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 df = pd.read_csv("../Donnees/game_data.csv")
@@ -27,8 +26,22 @@ def traitement_prix(prix):
     return
 
 
+def traitement_review_count(rc):
+    try:
+        return int(rc)
+    except ValueError:
+        return ""
+
+
+def traitement_description(desc):
+    try:
+        return desc[4:]
+    except IndexError:
+        return ""
+
+
 def traitement(dfcol, f):
-    remplacement = np.zeros(len(dfcol))
+    remplacement = [0] * len(dfcol)
 
     for i, el in enumerate(dfcol):
         remplacement[i] = f(el)
@@ -37,5 +50,9 @@ def traitement(dfcol, f):
 
 
 df["price"] = traitement(df["price"], traitement_prix)
+
+df["review_count"] = traitement(df["review_count"], traitement_review_count)
+
+df["description"] = traitement(df["description"], traitement_description)
 
 df.to_csv("../Donnees/cleaned_data.csv")
